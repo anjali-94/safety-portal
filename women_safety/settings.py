118440656a84839e5e79076ad31ef27c4f52e04c
anjali-94 <anjalishr987@gmail.com>
2024-11-29 04:21:06 +0530

    second commit

--- women_safety/settings.py
+++ women_safety/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-eqs!88n+vilwbxb8chy909qgsa#1*g#dur@a&e1+4on0%gq42*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app' , '.now.sh']
 
 
 # Application definition
@@ -122,7 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+
+import os
+STATICFILES_DIRS = os.path.join(BASE_DIR / "static"),
+STATIC_ROOT = os.path.join(BASE_DIR / "staticfiles_build", "static")
 
 
 # Default primary key field type
